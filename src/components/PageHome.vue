4c1cbe896616c8015b88892239747ad0904e2c71
Shielarose Gandhi <shielarosegandhi@gmail.com>
2022-01-05 13:28:09 -0500

    add mock json data, create new homepage component and show data on the page

---
+++ src/components/PageHome.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>Hello from page home</div>
+
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from '@/data.json';
+
+export default {
+  data(){
+    return {
+      threads: data.threads,
+      posts: data.posts,
+      users: data.users
+    }
+  },
+  methods: {
+    postById(postId){
+      return this.posts.find(post => post.id === postId);
+    },
+    userById(userId){
+      return this.users.find(user => user.id === userId);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Create a variable to store our Vue instance
+const forumApp = createApp(App)
+
+// Able to extendd and configure our instance before we mount to the DOM
+
+// Register a global component using the component instance function
+// forumApp.component('NiceButton', {})
+
+// Install a global plugin using the 'use' function
+// forumApp.use(SomePlugin)
+
+// Mount our Vue instance to the DOM 
+forumApp.mount('#app')

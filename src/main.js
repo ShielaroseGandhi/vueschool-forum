82b2b5058f9e24b51589df9309406ffd43d0d5e6
Shielarose Gandhi <shielarosegandhi@gmail.com>
2022-01-06 11:13:56 -0500

    create separate file for router

--- src/main.js
+++ src/main.js
@@ -1,27 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-	{
-		path: "/",
-    name: "Home",
-		component: PageHome,
-	},
-  {
-    path: "/thread/:id",
-    name: "ThreadShow",
-    component: PageThreadShow,
-    props: true
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+import router from '@/router'
 
 // Create a variable to store our Vue instance
 const forumApp = createApp(App)
